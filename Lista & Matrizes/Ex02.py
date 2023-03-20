63329d6ae2408b35a44a48d282250b93b3f818a6
D <98443721+RealDrinkee@users.noreply.github.com>
2023-03-20 17:08:40 -0300

    Add files via upload

---
+++ Lista & Matrizes/Ex02.py
@@ -0,0 +1,15 @@
+# Exercícios Listas
+#
+# Leia uma matriz 3 x 3, conte e escreva quantos valores maiores que 10 ela possui
+#
+# Declare uma matriz 5 x 5. Preencha com 1 a diagonal principal e com 0 os demais elementos.
+# Escreva ao final a matriz obtida
+#
+# Leia uma matriz de 3 x 3 elementos. Calcule a soma dos elementos que estão nas diagonais.
+#
+# Leia uma matriz 10 x 3 com as notas de 10 alunos em 3 provas. Em seguida, escreva o numero de
+# alunos cuja pior nota foi na prova 1, o número de alunos cuja pior nota foi na prova 2, e o número
+# de alunos cuja pior nota foi na prova 3. Em caso de empate das piores notas de um aluno, o
+# critério de desempate é arbitrário (de sua vontade), mas o aluno deve ser contabilizado apenas
+# uma vez.
+
